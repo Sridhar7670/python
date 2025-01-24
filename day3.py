cb85d3beed3db3ba4708917724416d4e1e10d7ff
Sridhar7670 <140085461+Sridhar7670@users.noreply.github.com>
2025-01-24 15:16:42 +0530

    Update day3.py
    added patterns in python

--- day3.py
+++ day3.py
@@ -15,13 +15,24 @@ for i in range (1,n+1):
     for j in range(1,n+1):
         print(j,end=" ")
     print()
-
+    
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# 1 2 3 4 5 
 
 n=int(input())
 for i in range (1,n+1):
     for j in range(1,n+1):
         print(i,end=" ")
     print()
+    
+# 1 1 1 1 1 
+# 2 2 2 2 2 
+# 3 3 3 3 3 
+# 4 4 4 4 4 
+# 5 5 5 5 5 
 
 
 n= int(input())
@@ -30,6 +41,12 @@ for i in range (1,n+1):
         print(i,end=' ')
     print()
 
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5 
+
 
 n= int(input())
 for i in range (1,n+1):
@@ -37,20 +54,11 @@ for i in range (1,n+1):
         print(j,end=' ')
     print()
 
-n= int(input())
-for i in range(1,n+1):
-    print("  "*(n-i),end='')
-    for j in range(1,i+1):
-        print(j,end=" ")
-    print()
-
-
-n= int(input())
-for i in range(1,n+1):
-    print("  "*(n-i),end='')
-    for j in range(1,i+1):
-        print(i,end=" ")
-    print()
+# 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5 
 
 
 n= int(input())
@@ -60,7 +68,11 @@ for i in range(1,n+1):
         print(j,end=" ")
     print()
 
-
+#         1 
+#       1 2 
+#     1 2 3 
+#   1 2 3 4 
+# 1 2 3 4 5
 
 n= int(input())
 for i in range(1,n+1):
@@ -68,7 +80,12 @@ for i in range(1,n+1):
     for j in range(i+1,1,-1):
         print(i,end=" ")
     print()
-
+    
+#         1 
+#       2 2 
+#     3 3 3 
+#   4 4 4 4 
+# 5 5 5 5 5 
 
 n= int(input())
 for i in range(1,n+1):
@@ -76,3 +93,85 @@ for i in range(1,n+1):
     for j in range(i+1,1,-1):
         print(j-1,end=" ")
     print()
+    
+#         1 
+#       2 1 
+#     3 2 1 
+#   4 3 2 1 
+# 5 4 3 2 1
+
+n= int(input())
+for i in range(1,n+1):
+    print("  "*(n-i),end='')
+    for j in range(i+1,1,-1):
+        print(j-1,end=' ')
+    print()
+    
+#         1 
+#       2 1 
+#     3 2 1 
+#   4 3 2 1 
+# 5 4 3 2 1
+
+n= int(input())
+for i in range(1,n+1):
+    print("  "*(n-i),end='')
+    for j in range(n,n-i,-1):
+        print(j,end=' ')
+    print()
+
+#         5 
+#       5 4 
+#     5 4 3 
+#   5 4 3 2 
+# 5 4 3 2 1
+
+n= int(input())
+for i in range(1,n+1):
+    print(" "*(n-i),end='')
+    for j in range(n,n-i,-1):
+        print(j,end=' ')
+    print()
+    
+#     5 
+#    5 4 
+#   5 4 3 
+#  5 4 3 2 
+# 5 4 3 2 1    
+
+n= int(input())
+for i in range(n,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(i):
+        print('*',end=' ')
+    print()
+    
+# * * * * * 
+#  * * * * 
+#   * * * 
+#    * * 
+#     *
+
+
+n= int(input())
+for i in range(1,n+1):
+    print(" "*(n-i),end='')
+    for j in range(n,n-i,-1):
+        print('*',end=' ')
+    print()
+for i in range(n,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(i):
+        print('*',end=' ')
+    print()
+
+#     * 
+#    * * 
+#   * * * 
+#  * * * * 
+# * * * * * 
+# * * * * * 
+#  * * * * 
+#   * * * 
+#    * * 
+#     *
