566f9574b3e3cc48657aa0c2431b33d80bb7d094
Sridhar7670 <140085461+Sridhar7670@users.noreply.github.com>
2025-01-24 15:27:03 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -175,3 +175,25 @@ for i in range(n,0,-1):
 #   * * * 
 #    * * 
 #     *
+
+n= int(input())
+for i in range(1,n+1):
+    print(" "*(n-i),end='')
+    for j in range(n,n-i,-1):
+        print('*',end=' ')
+    print()
+for i in range(n-1,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(i):
+        print('*',end=' ')
+    print()
+
+#     * 
+#    * * 
+#   * * * 
+#  * * * * 
+# * * * * * 
+#  * * * * 
+#   * * * 
+#    * * 
+#     * 
