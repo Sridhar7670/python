7744917a870f989a80e10cb70dc46bf031647c72
Sridhar7670 <140085461+Sridhar7670@users.noreply.github.com>
2025-01-29 15:20:06 +0530

    Update day4.py

--- day4.py
+++ day4.py
@@ -26,3 +26,28 @@ s=0
 for i in l:
     s+=i
 print('sum is ',s)
+
+
+
+
+#delete duplicates from the list 
+l = list(map(int, input('Enter the values separated by space: ').split()))
+i = 0
+while i < len(l):
+    j = i + 1
+    while j < len(l):
+        if l[i] == l[j]:
+            l.pop(j)
+        else:
+            j += 1
+    i += 1
+print(l)
+#delete duplicates using external memory 
+
+l = list(map(int, input('enter the values separated by space').split()))
+new=[]
+for i in l:
+    if i not in new:
+        new.append(i)
+print(new)
+        
