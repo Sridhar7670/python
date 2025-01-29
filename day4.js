f495836ec662d70b66d6c270d14359867ec98851
Sridhar7670 <140085461+Sridhar7670@users.noreply.github.com>
2025-01-29 16:00:18 +0530

    Update day4.js

--- day4.js
+++ day4.js
@@ -80,3 +80,14 @@ console.log(sum)
     }
 console.log(arr)
 
+function areAnagrams(str1, str2) {
+    str1 = str1.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    str2 = str2.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+    if (str1.length !== str2.length) {
+        return false;
+    }
+    return str1.split('').sort().join('') === str2.split('').sort().join('');
+}
+
+console.log(areAnagrams('listen', 'silent')); // true
